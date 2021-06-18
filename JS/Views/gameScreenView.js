9dc2ebd291cb476d7d084bdd05062cf997c45d28
sbird389 <s.bird389@gmail.com>
2021-06-18 14:35:03 +0100

    Added Countdown Timer and StartTimer

--- JS/Views/gameScreenView.js
+++ JS/Views/gameScreenView.js
@@ -5,6 +5,31 @@ class GameScreenView {
   toggleHidden() {
     this._parentElement.classList.toggle("u-hidden");
   }
-}
 
+  countdownTimer() {
+    let i = 3;
+    const updateTimer = function () {
+      if (i == 1) {
+        document.getElementById("message").textContent = "GO!";
+        clearInterval(timer);
+        return;
+      }
+      i--;
+      document.getElementById("message").textContent = i;
+      console.log(i);
+    };
+    const timer = setInterval(updateTimer, 1000);
+  }
+
+  timerRunDown() {
+    const timer = setInterval(() => {
+      this._parentElement.querySelector(".timer").textContent =
+        this._parentElement.querySelector(".timer").textContent.slice(0, -1);
+
+      if (this._parentElement.querySelector(".timer").textContent.length == 0) {
+        clearInterval(timer);
+      }
+    }, 200);
+  }
+}
 export default new GameScreenView();
--- JS/controller.js
+++ JS/controller.js
@@ -4,6 +4,8 @@ import startMenuView from "./Views/startMenuView.js";
 const controlGameStart = function () {
   console.log("Game Start");
   gameScreenView.toggleHidden();
+  //gameScreenView.timerRunDown();
+  gameScreenView.countdownTimer();
 };
 
 startMenuView.startClickHandler(controlGameStart);
